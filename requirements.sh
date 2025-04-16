0b92242e8d1063101d521cce39873ead62e8b753
Vikas Goel <vikasx.goel@intel.com>
2025-04-16 16:23:34 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+#Commands used to install the libraries in Ubuntu: 
+sudo apt-get update
+sudo apt-get install python3 python3-pip
+sudo pip3 install pandas streamlit nltk
+
+
+#Type python3 in command prompt and use the below command to download nltk.('punkt'):
+>> nltk.download('punkt')
